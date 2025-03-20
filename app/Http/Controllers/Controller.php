2282c5b3c70934c4c029d280290b6755bc80cc75
Sergey <1@php-cat.com>
2025-03-20 10:40:50 +0500

    23

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,10 +6,10 @@ namespace App\Http\Controllers;
  * @OA\Info(
  *      title="My API",
  *      version="1.0.0",
- *      description="Описание API"
+ *      description="API для сенсора и получения истории"
  * )
  * @OA\Server(
- *      url="https://example.com/api",
+ *      url="https://test-sensor.php-cat.com",
  *  )
  * @OA\Server(
  *      url="https://uber.local",
